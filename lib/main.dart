af6fc47beb0e0b3b2299770029d373be0f578cb2
Abdellatif Amireh <abdellatif.amireh95@gmail.com>
2022-03-23 09:33:17 +0400

    add sos test

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application. ok thanks
+  // This widget is the root of your application. ok thankssos
   @override
   Widget build(BuildContext context) {
     //---------------CupertinoApp START-------------------- no
